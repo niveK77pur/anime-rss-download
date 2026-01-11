5968fd17a307557a046a7c4241d0b5cf47ce8dff
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2026-01-11 15:38:54 +0100

    feat: Fetch and filter RSS anime to download

--- main.go
+++ main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"log/slog"
+	"regexp"
+
+	"github.com/mmcdole/gofeed"
+	"github.com/pterm/pterm"
+)
+
+var logger = slog.New(pterm.NewSlogHandler(&pterm.DefaultLogger))
 
 func main() {
-	fmt.Println("Hello Nix!")
+	pterm.DefaultLogger.Level = pterm.LogLevelDebug
+
+	fp := gofeed.NewParser()
+	feed, _ := fp.ParseURL("https://subsplease.org/rss/?r=1080")
+
+	rexs, err := getRegexies()
+	if err != nil {
+		return
+	}
+
+	toDownload := filterToDownload(feed.Items, rexs)
+
+	for _, item := range toDownload {
+		logger.Debug(item.Title)
+	}
+}
+
+func getRegexies() ([]*regexp.Regexp, error) {
+	// TODO: Fetch from config file
+	var download_regexies = []string{"Jujutsu Kaisen", "Fate Strange Fake"}
+
+	var regexies []*regexp.Regexp
+	for _, regex_string := range download_regexies {
+		re, err := regexp.Compile(regex_string)
+		if err != nil {
+			logger.Error("Failed to compile regex", "regex", regex_string)
+			return nil, errors.New("Failed to compile regex")
+		}
+		regexies = append(regexies, re)
+	}
+	return regexies, nil
+}
+
+func filterToDownload(items []*gofeed.Item, regexies []*regexp.Regexp) (toDownload []*gofeed.Item) {
+	for _, item := range items {
+		for _, re := range regexies {
+			if re.MatchString(item.Title) {
+				toDownload = append(toDownload, item)
+				break
+			}
+		}
+	}
+	return
 }
